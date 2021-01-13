bbba3bd6ef7529ead3ab0b5235b68bb311ae2ccc
verzelea <arthurverzele@hotmail.com>
2021-01-13 16:32:36 +0100

    :construction_worker: update procfile

--- conduit/settings.py
+++ conduit/settings.py
@@ -25,6 +25,12 @@ class Config(object):
         'http://localhost:4200',
         'http://0.0.0.0:4000',
         'http://localhost:4000',
+        'https://devoptpfinal.herokuapp.com/api/articles',
+        'https://devoptpfinal.herokuapp.com',
+        'http://0.0.0.0:5432',
+        'http://localhost:5432',
+        'http://0.0.0.0:' + os.environ.get('PORT', '8080'),
+        'https://0.0.0.0:' + os.environ.get('PORT', '8080'),
     ]
     JWT_HEADER_TYPE = 'Token'
 
